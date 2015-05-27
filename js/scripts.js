363ee768dc74fbc9c8adeef1e5cc8e804d124966
Alec Arme & cleri philiastides <student@epicodus.com>
2015-05-27 14:51:22 -0700

    Add spec for numbers less than 6. Passes spec.

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,24 @@ var primeSift = function(number) {
   for(var i = 2; i < number; i++) {
     numberList.push(i);
   };
-  return numberList;
+
+var finalNumberList = numberList.slice();
+
+  for(var i = 0; i <= numberList.length; i++) {
+    var currentNum = numberList[i];
+      for(var prime = 2; prime < number; prime++) {
+
+        if (currentNum === prime) {
+        } else if (currentNum % prime === 0) {
+           finalNumberList.splice(i, 1);
+        }
+      }
+  }
+
+return finalNumberList;
+
+
+
+
+  // return numberList;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('primeSift', function() {
   it("returns the prime numbers less than 4", function() {
     expect(primeSift(4)).to.deep.equal([2,3]);
   });
+  it("returns the prime numbers less than 6", function() {
+    expect(primeSift(6)).to.deep.equal([2,3,5]);
+  });
 });
