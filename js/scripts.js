1189cfe4431d7f2cd966a84dc0e9b558b89bf0cf
Alec Arme & cleri philiastides <student@epicodus.com>
2015-05-27 16:06:25 -0700

    Refactor to remove spliceFlag and use break; instead

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var primeSift = function(number) {
   var numberList = []
   var spliceCounter = 0
-  var spliceFlag = true
   for(var i = 2; i <= number; i++) {
     numberList.push(i);
   };
@@ -10,17 +9,29 @@ var finalNumberList = numberList.slice(); // allows us to modify new array witho
 
   for(var i = 0; i < numberList.length; i++) {
     var currentNum = numberList[i];
-      spliceFlag = true;
       for(var prime = 2; prime < currentNum; prime++) {  // prime is the index of the loop
-        if (spliceFlag === true) {
-           if (currentNum % prime === 0) {  // currentNum is looking at the original numberList
-             finalNumberList.splice(i-spliceCounter, 1);
-             spliceCounter++;
-             spliceFlag = false; // forces (effective) exit of loop
+         if (currentNum % prime === 0) {  // currentNum is looking at the original numberList
+           finalNumberList.splice(i-spliceCounter, 1);
+           spliceCounter++;
+           break; // forces exit of loop
           }
-        }
       }
     }
 
 return finalNumberList;
 };
+
+$(document).ready(function() {
+  $('form#number').submit(function(event) {
+    $('#result').show();
+    var inputNum = ($('input#number')).val();
+    var resultNums = primeSift(inputNum);
+
+    resultNums = resultNums.join(', ');
+
+    $(".result_nums").text(resultNums);
+
+
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -7,15 +7,14 @@ describe('primeSift', function() {
   });
   it("returns the prime numbers up to 10", function() {
     expect(primeSift(10)).to.eql([2,3,5,7]);
-  })
+  });
   it("returns the prime numbers for any number", function() {
     expect(primeSift(56)).to.eql([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53])
-  })
+  });
   it("returns the prime numbers for any number", function() {
     expect(primeSift(937).length).to.eql(159)
-  })
+  });
   it("returns the prime numbers when the target number is prime", function () {
     expect(primeSift(17)).to.eql([2,3,5,7,11,13,17]);
   });
-
 });
