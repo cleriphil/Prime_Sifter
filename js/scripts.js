70c6dd5a633f8ae8ed1b892fc8c1557934b264f1
Alec Arme & cleri philiastides <student@epicodus.com>
2015-05-27 14:20:03 -0700

    Create initial files. Add spec for primes less than 4. Passes spec.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var primeSift = function(number) {
+  var numberList = []
+  for(var i = 2; i < number; i++) {
+    numberList.push(i);
+  };
+  return numberList;
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('primeSift', function() {
+  it("returns the prime numbers less than 4", function() {
+    expect(primeSift(4)).to.deep.equal([2,3]);
+  });
+});
