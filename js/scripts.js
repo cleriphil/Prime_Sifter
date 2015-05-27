c1348df6809e03188b74fc66547e2f1216f0cd68
Alec Arme & cleri philiastides <student@epicodus.com>
2015-05-27 15:24:44 -0700

    Add specs for numbers less than 10 and 'any number'. Passes spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 var primeSift = function(number) {
   var numberList = []
+  var spliceCounter = 0
+  var spliceFlag = true
   for(var i = 2; i < number; i++) {
     numberList.push(i);
   };
 
-var finalNumberList = numberList.slice();
+var finalNumberList = numberList.slice(); // allows us to modify new array without also modifying origional
 
-  for(var i = 0; i <= numberList.length; i++) {
+  for(var i = 0; i < numberList.length; i++) {
     var currentNum = numberList[i];
-      for(var prime = 2; prime < number; prime++) {
-
-        if (currentNum === prime) {
-        } else if (currentNum % prime === 0) {
-           finalNumberList.splice(i, 1);
+      spliceFlag = true;
+      for(var prime = 2; prime < currentNum; prime++) {  // prime is the index of the loop
+        if (spliceFlag === true) {
+           if (currentNum % prime === 0) {  // currentNum is looking at the original numberList
+             finalNumberList.splice(i-spliceCounter, 1);
+             spliceCounter++;
+             spliceFlag = false; // forces (effective) exit of loop
+          }
         }
       }
-  }
+    }
 
 return finalNumberList;
-
-
-
-
-  // return numberList;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,14 @@
 describe('primeSift', function() {
   it("returns the prime numbers less than 4", function() {
-    expect(primeSift(4)).to.deep.equal([2,3]);
+    expect(primeSift(4)).to.eql([2,3]);
   });
   it("returns the prime numbers less than 6", function() {
-    expect(primeSift(6)).to.deep.equal([2,3,5]);
+    expect(primeSift(6)).to.eql([2,3,5]);
   });
+  it("returns the prime numbers less than 10", function() {
+    expect(primeSift(10)).to.eql([2,3,5,7]);
+  })
+  it("returns the prime numbers for any number", function() {
+    expect(primeSift(56)).to.eql([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53])
+  })
 });
